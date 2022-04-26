91b0c9bf1f2aef1d18c288a26b36cb3c8feecebb
KremnevViacheslav <rusted2192@gmail.com>
2022-04-27 00:08:20 +0300

    Initial Commit

---
+++ AuthorizationAppV2/AuthorizationAppV2App.swift
@@ -0,0 +1,17 @@
+//
+//  AuthorizationAppV2App.swift
+//  AuthorizationAppV2
+//
+//  Created by Вячеслав Кремнев on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AuthorizationAppV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
